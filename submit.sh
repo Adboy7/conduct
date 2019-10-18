2cc891ce561fbf79cd48d698512c52ae6b09ec02
Adboy7 <chiodoadrien@gmail.com>
2019-10-18 07:29:45 -0700

    add submit

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=test
+#SBATCH --output=res.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+echo test
+OMP_NUM_THREADS=2 ./conducting 1 50 0.4 10
